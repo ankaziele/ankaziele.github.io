4541f6a1e1a455709afe10d489de22b21c599f25
ankaziele <ankaziele@gmail.com>
2018-10-04 16:09:28 +0200

    add position fixed to hero image - larage screen

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var faceImage = document.querySelector('.face');
+// var faceImage = document.querySelector('.face');
 
-window.addEventListener('scroll', function(){
-   var screenOffset = window.scrollY;
-    var faceOffset = screenOffset * 0.5; //predkosc przesuwania
+// window.addEventListener('scroll', function(){
+//    var screenOffset = window.scrollY;
+//     var faceOffset = screenOffset * 0.5; //predkosc przesuwania
 
-    faceImage.style.paddingTop = (faceOffset) + 'px';
+//     faceImage.style.paddingTop = (faceOffset) + 'px';
     
     
-})
+// })
 
