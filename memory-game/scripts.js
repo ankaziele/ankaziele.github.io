8d07d967ea341b95b09edb3baf866390bd3fa811
ankaziele <ankaziele@gmail.com>
2018-10-04 11:52:33 +0200

    memory game working

--- memory-game/scripts.js
+++ memory-game/scripts.js
@@ -1,7 +1,74 @@
 const cards = document.querySelectorAll('.memory-card');
 
-function flipCard(){
-  this.classList.toggle('flip');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard;
+let secondCard;
+
+
+
+function flipCard() {
+  if (lockBoard) return;
+  if(this === firstCard) return;
+
+  this.classList.add('flip');
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+  //second click
+  secondCard = this;
+
+
+  checkForMatch();
+
 }
+
+function checkForMatch() {
+
+  //do cards match?
+
+  let isMatch = firstCard.dataset.hobby === secondCard.dataset.hobby
+
+  isMatch ? disableCards() : unflipCards();
+
+}
+
+function disableCards() {
+  // it's a match!
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+
+function unflipCards() {
+  lockBoard = true;
+  //not a match :(
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+        resetBoard();
+  }, 1500)
+}
+
+function resetBoard(){
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+
+}
+
+(function shuffle(){
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random()*12);
+  card.style.order =  randomPos;
+});
+})();
+
 
 cards.forEach(card => card.addEventListener('click', flipCard))
