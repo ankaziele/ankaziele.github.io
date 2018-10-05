ecbe2fe1a2b171f4a77bdf09aeb5c196c2b50a74
ankaziele <ankaziele@gmail.com>
2018-10-05 05:11:46 +0200

    toggle classes add

--- main.js
+++ main.js
@@ -9,3 +9,22 @@
     
 // })
 
+
+
+function toggleClasses(element, classes, time){
+    var index = 0;
+    setInterval(function(){
+        element.classList.remove(
+            index !== 0 ? 
+            classes[index-1] : classes[classes.length-1]
+        );
+        element.classList.add(classes[index]);
+        index = (index + 1) % classes.length
+    }, time)
+}
+
+toggleClasses(
+    document.querySelector('.marketing'),
+    ['one', 'two'],
+    3000
+)
