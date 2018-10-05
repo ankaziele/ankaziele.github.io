e722e742ec2f216b38c9b59f5e6cb15b8a77b877
ankaziele <ankaziele@gmail.com>
2018-10-05 06:45:18 +0200

    WIP carousel p.1

--- main.js
+++ main.js
@@ -28,3 +28,45 @@ toggleClasses(
     ['one', 'two'],
     3000
 )
+
+//about me carousel
+
+var previousButton = document.querySelector('.features-prevoius-button');
+var nextButton = document.querySelector('.features-next-button');
+var navigation = document.querySelector('.about-navigation');
+
+nextButton.addEventListener('click', function(){
+    clearInterval(intervalId);
+    activateNextSlide()
+});
+previousButton.addEventListener('click', function(){
+    clearInterval(intervalId);
+    activatePreviousSlide()
+});
+
+var intervalId = setInterval(function(){
+    activateNextSlide()
+}, 2000);
+
+function activateNextSlide(){
+    var element = document.querySelector('.features div:first-child');
+    element.parentElement.appendChild(element);
+
+    var current = navigation.querySelector('active');
+    var next = current.nextElementSibling || navigation.querySelector('span');
+
+    current.classList.remove('active');
+    next.classList.add('active');
+}
+
+
+function activatePreviousSlide() {
+  var element = document.querySelector('.features div:last-child');
+  element.parentElement.prepend(element);
+
+  var current = navigation.querySelector('.active');
+  var previous = current.previousElementSibling || navigation.querySelector('span:last-child');
+
+  current.classList.remove('active');
+  previous.classList.add('active');
+}
