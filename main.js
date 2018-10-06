20bbde9a805c51eb7b499ce01400a55327d4da36
ankaziele <ankaziele@gmail.com>
2018-10-06 10:46:28 +0200

    remove fixed position of main image

--- main.js
+++ main.js
@@ -24,49 +24,49 @@ function toggleClasses(element, classes, time){
 }
 
 toggleClasses(
-    document.querySelector('.marketing'),
+    document.querySelector('.code'),
     ['one', 'two'],
-    3000
+    2000
 )
 
 //about me carousel
 
-var previousButton = document.querySelector('.features-prevoius-button');
-var nextButton = document.querySelector('.features-next-button');
-var navigation = document.querySelector('.about-navigation');
+// var previousButton = document.querySelector('.features-prevoius-button');
+// var nextButton = document.querySelector('.features-next-button');
+// var navigation = document.querySelector('.about-navigation');
 
-nextButton.addEventListener('click', function(){
-    clearInterval(intervalId);
-    activateNextSlide()
-});
-previousButton.addEventListener('click', function(){
-    clearInterval(intervalId);
-    activatePreviousSlide()
-});
+// nextButton.addEventListener('click', function(){
+//     clearInterval(intervalId);
+//     activateNextSlide()
+// });
+// previousButton.addEventListener('click', function(){
+//     clearInterval(intervalId);
+//     activatePreviousSlide()
+// });
 
-var intervalId = setInterval(function(){
-    activateNextSlide()
-}, 2000);
+// var intervalId = setInterval(function(){
+//     activateNextSlide()
+// }, 2000);
 
-function activateNextSlide(){
-    var element = document.querySelector('.features div:first-child');
-    element.parentElement.appendChild(element);
+// function activateNextSlide(){
+//     var element = document.querySelector('.features div:first-child');
+//     element.parentElement.appendChild(element);
 
-    var current = navigation.querySelector('active');
-    var next = current.nextElementSibling || navigation.querySelector('span');
+//     var current = navigation.querySelector('active');
+//     var next = current.nextElementSibling || navigation.querySelector('span');
 
-    current.classList.remove('active');
-    next.classList.add('active');
-}
+//     current.classList.remove('active');
+//     next.classList.add('active');
+// }
 
 
-function activatePreviousSlide() {
-  var element = document.querySelector('.features div:last-child');
-  element.parentElement.prepend(element);
+// function activatePreviousSlide() {
+//   var element = document.querySelector('.features div:last-child');
+//   element.parentElement.prepend(element);
 
-  var current = navigation.querySelector('.active');
-  var previous = current.previousElementSibling || navigation.querySelector('span:last-child');
+//   var current = navigation.querySelector('.active');
+//   var previous = current.previousElementSibling || navigation.querySelector('span:last-child');
 
-  current.classList.remove('active');
-  previous.classList.add('active');
-}
+//   current.classList.remove('active');
+//   previous.classList.add('active');
+// }
